550a90e75ff1a69adb73a404bfc51febd32ac06c
manulangat <emmanuelthedeveloper@gmail.com>
2020-06-26 22:44:24 +0300

    too tired to go on

--- app.py
+++ app.py
@@ -27,6 +27,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app,db)
 bcrypt = Bcrypt(app)
 import datetime
+import jwt
 class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer,primary_key=True,autoincrement=True)
@@ -43,6 +44,35 @@ class User(db.Model):
         )
         self.registered_on = datetime.datetime.now()
         self.admin = admin
+    def encode_auth_token(self,user_id):
+        try:
+            payload = {
+                'exp':datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
+                'iat': datetime.datetime.utcnow(),
+            'sub': user_id
+            }
+            return jwt.encode(
+                payload,
+                os.environ['SECRET_KEY'],
+                algorithm='HS256'
+            )
+        except Exception as e:
+            return e
+    @staticmethod
+    def decode_auth_token(auth_token):
+        """
+        Decodes the auth token
+        :param auth_token:
+        :return: integer|string
+        """
+        try:
+            payload = jwt.decode(auth_token, os.environ.get('SECRET_KEY'))
+            return payload['sub']
+        except jwt.ExpiredSignatureError:
+            return 'Signature expired. Please log in again.'
+        except jwt.InvalidTokenError:
+            return 'Invalid token. Please log in again.'
+
 class Products(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(200))

7c272e21bfe04b9fdde01395d6931d536771d329
manulangat <emmanuelthedeveloper@gmail.com>
2020-06-26 22:16:24 +0300

    ft-user_authentication-backend

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask,jsonify,render_template,request,url_for,redirect
 from flask_sqlalchemy import SQLAlchemy 
 from flask_migrate import Migrate
+
+from flask_bcrypt import Bcrypt
 import os 
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
